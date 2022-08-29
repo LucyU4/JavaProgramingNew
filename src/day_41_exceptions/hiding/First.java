04daa48eac9cae44b7edc37f54ba8d377b56a250
Luzvin Urban <lucybdk@gmail.com>
2022-08-29 11:08:29 -0400

    8/26/Class

---
+++ src/day_41_exceptions/hiding/First.java
@@ -0,0 +1,14 @@
+package day_41_exceptions.hiding;
+
+public class First {
+    public static void  staticMethod(){
+        System.out.println("Static method from First Class");
+    }
+    public void instanceMethodA(){
+        staticMethod();
+
+    }
+public void instanceMethodB(){
+        staticMethod();
+}
+}
---
+++ src/day_41_exceptions/hiding/Second.java
@@ -0,0 +1,12 @@
+package day_41_exceptions.hiding;
+
+public class Second extends First{
+
+    public static void staticMethod(){
+        System.out.println("Static metod from Second");
+    }
+    public void instanceMethod(){
+        staticMethod();
+
+    }
+}
